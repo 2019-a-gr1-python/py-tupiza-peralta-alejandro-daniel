9fd95493c0496ad7345d30c5eaa47a9fd49dd3ff
Alejandro <ad_tp@hotmail.com>
2019-05-22 08:15:50 -0500

    data frames

--- 03_Pandas/B_DataFrames.py
+++ 03_Pandas/B_DataFrames.py
@@ -10,12 +10,16 @@ import pandas as pd
 #array randomico
 arr_rand = np.random.randint(0,10,6).reshape(2,3)
 df = pd.DataFrame(arr_rand,
-                  columns=['Estatura (cm)','Peso (gr)','Edad (anios)'])
+                  columns=['Estatura (cm)','Peso (gr)','Edad (anios)'],
+                  index=['Adrian','Vicente'])
+
+df['Estatura (cm)']['Adrian']
 
 df2 = pd.DataFrame(arr_rand)
 df3 = pd.DataFrame(arr_rand)
 
 df2.columns = ['Estatura (cm)','Peso (gr)','Edad (anios)']
+df2.index = ['Adrian','Vicente']
 
 df3[0] # No es el indice
 df2['Estatura (cm)'] # Es el nombre de la columna
