9226607808b6907c6de195ac752a82cd81a6d606
Alejandro <ad_tp@hotmail.com>
2019-05-22 08:29:02 -0500

    pickle

--- 03_Pandas/C_lectura_csv.py
+++ 03_Pandas/C_lectura_csv.py
@@ -14,9 +14,26 @@ import os
 
 path = 'C:/Users/Alejo/Documents/Documentos_U/Python/py-tupiza-peralta-alejandro-daniel/03_Pandas/data/csv/artwork_data.csv'
 
+columnas_a_usar = ['id','artist','title',
+                   'medium','year','acquisitionYear',
+                   'height','width','units']
+
 df = pd.read_csv(
         path,
         nrows=100,
-        usecols=['id','artist'],
+        usecols=columnas_a_usar,
         index_col='id'
         )
+
+df_completo = pd.read_csv(
+        path,
+        usecols=columnas_a_usar,
+        index_col='id'
+        )
+
+path_guardado = 'C:/Users/Alejo/Documents/Documentos_U/Python/py-tupiza-peralta-alejandro-daniel/03_Pandas/data/csv/artwork_data.pickle'
+
+df_completo.to_pickle(path_guardado)
+
+df_completo_pickle = pd.read_pickle()
+
