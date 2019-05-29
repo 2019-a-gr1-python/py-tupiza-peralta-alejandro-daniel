6b4e9a5bfd85fe5879a16eb9be59c0ad7d7a41f7
Alejandro <ad_tp@hotmail.com>
2019-05-29 07:56:07 -0500

    Update F_iloc_loc.py

--- 03_Pandas/F_iloc_loc.py
+++ 03_Pandas/F_iloc_loc.py
@@ -5,3 +5,24 @@ Created on Wed May 29 07:35:53 2019
 @author: Alejo
 """
 
+import pandas as pd
+
+path_guardado = 'C:/Users/Alejo/Documents/Documentos_U/Python/py-tupiza-peralta-alejandro-daniel/03_Pandas/data/csv/artwork_data.pickle'
+
+df = pd.read_pickle(path_guardado)
+
+primero = df.loc[1035,'artist']
+segundo = df.loc[1036,'units']
+
+df.loc[0]   # Error porque no esta dentro del label indice
+
+primero_a = df.iloc[0,1]
+
+primero_b = df.iloc[0,:]
+
+primero_c = df.iloc[0:100,2:4]
+
+a = df['width'].sort_values()
+
+
+
