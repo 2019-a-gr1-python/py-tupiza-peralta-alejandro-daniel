9b5d1687194f62a0a741db87a2cebbb4c47a791a
Alejandro <ad_tp@hotmail.com>
2019-05-29 08:11:06 -0500

    cambios iloc y loc

--- 03_Pandas/F_iloc_loc.py
+++ 03_Pandas/F_iloc_loc.py
@@ -22,7 +22,22 @@ primero_b = df.iloc[0,:]
 
 primero_c = df.iloc[0:100,2:4]
 
-a = df['width'].sort_values()
+tres_primeros = df.head(10)['width'].sort_values(ascending=False).head(3)
 
+tres_ultimos = df.head(10)['width'].sort_values().tail(3)
+
+a = df['year'].sort_values(axis=0)
+
+serie_validado = pd.to_numeric(df['width'], errors='coerce')
+
+df.loc[:,'width'] = serie_validado
+df.iloc[:,5] = serie_validado
+
+diez_primeros = df['width'].sort_values(ascending=False).head(10)
+
+diez_ultimos = df['width'].sort_values(ascending=False).tail(10)
+
+serie_validado_height = pd.to_numeric(df['height'], errors='coerce')
+df.loc[:,'height'] = serie_validado_height
 
 
