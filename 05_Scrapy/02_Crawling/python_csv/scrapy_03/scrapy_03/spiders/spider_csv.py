1894429f4f448fa6c24b37b000688c4b499d5d39
Alejo Tupiza <ad_tp@hotmail.com>
2019-07-03 07:32:08 -0500

    csv headers

---
+++ 05_Scrapy/02_Crawling/python_csv/scrapy_03/scrapy_03/spiders/spider_csv.py
@@ -0,0 +1,32 @@
+import scrapy
+from scrapy.spiders import CSVFeedSpider
+
+# Spider
+# CrawlSpider
+# CSVFeedSpider
+
+class VinosArania(CSVFeedSpider):
+    name = 'vinos'
+
+    start_urls = [
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
+    ]
+
+    delimiter = ';'
+
+    quotechar = '"'
+
+    headers = [
+        'fixed density',
+        'volatile acidity',
+        'citric acid',
+        'residual sugar',
+        'chlorides',
+        'free sulfur dioxide',
+        'total sulfur dioxide',
+        'density',
+        'pH',
+        'sulphates',
+        'alcohol',
+        'quality'
+    ]
