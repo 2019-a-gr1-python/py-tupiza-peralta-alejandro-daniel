8055ad5a1957627cb2b1e2348818c95d7a63f008
Alejo Tupiza <ad_tp@hotmail.com>
2019-07-03 07:09:35 -0500

    cambios libros

--- 05_Scrapy/02_Crawling/deber_libros/libros/libros/spiders/libros.py
+++ 05_Scrapy/02_Crawling/deber_libros/libros/libros/spiders/libros.py
@@ -4,36 +4,29 @@ from scrapy.linkextractors import LinkExtractor
 
 class araniaCrawlLibro(CrawlSpider):
     name = 'crawl_libro'
-    allowed_domains = [  # Heredado (conservar nombre)
-        'http://books.toscrape.com/'
+    allowed_domains = [     #Heredado conservar nombre de atributo
+        'toscrape.com'
     ]
-    start_urls = [  # Heredado (conservar nombre)
-        'http://books.toscrape.com/'
+    
+    start_urls=[    #Heredado conservar nombre de atributo
+        'http://books.toscrape.com/index.html'
     ]
-    # Heredado (conservar nombre)
 
-
-    url_segmento_permitido = (
-        '/catalogue/category/books/mystery_3/',
-        '/catalogue/category/books/fantasy_19/' 
+    url_segmentos_permitidos=(
+        'category/books/fantasy_19',
+        'category/books/mystery_3'
     )
-
-
-    regla_dos = (
-        Rule(
-            LinkExtractor(
-                allow_domains=allowed_domains,
-                allow=url_segmento_permitido
-            ), callback='parse_page')
-        ,
+        
+    rules=(
+        Rule(LinkExtractor(
+            allow_domains=allowed_domains,
+            allow=url_segmentos_permitidos
+        ),callback='parse_page'),
     )
-
-    rules = regla_dos
 
 
     def parse_page(self, response):
-        lista_libros = response.css('article.product_pod > h3 > a::attr(title)').extract()
-
-        for libreria in lista_programas:
+        lista_titulos = response.css('article.product_pod > h3 > a::attr(title)').extract()
+        for titulo in lista_titulos:
             with open('libros.txt', 'a+') as archivo:
-                archivo.write(libreria + '\n')
+                archivo.write(titulo + '\n')
