d6e829c1e38049e99d884a30b88b1b6389caaefb
Alejo Tupiza <ad_tp@hotmail.com>
2019-07-30 22:24:01 -0500

    filtrar precios en df

--- Deberes/Proyecto/df_impresoras.py
+++ Deberes/Proyecto/df_impresoras.py
@@ -5,3 +5,19 @@ Created on Tue Jul 30 21:46:32 2019
 @author: ad_tp
 """
 
+import pandas as pd
+import os
+
+path_impresoras = 'C:/Users/ad_tp/OneDrive/Documentos/GitHub/py-tupiza-peralta-alejandro-daniel/Deberes/Proyecto/proyecto_scrapy/scrapyP/impresoras_items.csv'
+
+columnas_a_usar = ['titulo','modelo','marca',
+                  'precio','condicion','opiniones',
+                   'tipo_vendedor','ventas_vendedor']
+
+df_impresoras = pd.read_csv(
+        path_impresoras,
+        usecols=columnas_a_usar,
+        index_col='titulo'
+        )
+
+precio = df_impresoras.filter(items = ["titulo","precio"]).sort_values('precio', ascending=False)
